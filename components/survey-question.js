0dba40ab44c08ae758cbe40b37dde310f03c9cfe
camdenhao <camden.hao@gmail.com>
2022-04-24 15:51:09 -0400

    transitions

--- components/survey-question.js
+++ components/survey-question.js
@@ -1,47 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight, Dimensions } from 'react-native';
 
-function surveyQuestion(props){
-    const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
-    const styles = StyleSheet.create({
-        questionContainer: {
-            backgroundColor: 'lightgray',
-            marginTop: 20,
-            flex: 1,
-            alignItems: 'center',
-            width: windowWidth
-        },
-        questionText: {
-            fontSize: 20,
-            padding: 30,
-            marginTop: 10,
-            marginBottom: 10,
-            height: 50
-        },
-        touchContainer: {
-            height: 80, 
-            width: 300,
-            borderRadius: 10,
-            margin: 8
-        }, 
-        answer: {
-            flex: 1,
-            justifyContent: 'center',
-            height: 80, 
-            width: 300,
-            borderRadius: 10,
-            backgroundColor: 'rgb(26,177,147)',
-        }, 
-        answerText: {
-            textAlign: 'center',
-            fontWeight: 'bold',
-            color: 'white',
-            padding: 15,
-            fontSize: 20
-        },
-    });
-    
+function surveyQuestion(props){    
     return (
     <View style={styles.questionContainer}>
         <Text style={styles.questionText}>{props.questionTitle}</Text>
@@ -68,5 +28,41 @@ function surveyQuestion(props){
     </View>
     );
 }
+const windowWidth = Dimensions.get('window').width;
+const styles = StyleSheet.create({
+    questionContainer: {
+        backgroundColor: 'lightgray',
+        marginTop: 0,
+        flex: 1,
+        alignItems: 'center',
+        width: windowWidth
+    },
+    questionText: {
+        fontSize: 20,
+        padding: 20,
+        marginTop: 10,
+        marginBottom: 10,
+        height: 'fit-content'
+    },
+    touchContainer: {
+        height: 60, 
+        width: 300,
+        borderRadius: 10,
+        margin: 8
+    }, 
+    answer: {
+        flex: 1,
+        justifyContent: 'center',
+        borderRadius: 10,
+        backgroundColor: 'rgb(26,177,147)',
+    }, 
+    answerText: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+        padding: 15,
+        fontSize: 20
+    },
+});
 
 export default surveyQuestion;
--- screens/survey.js
+++ screens/survey.js
@@ -1,108 +1,12 @@
 import React, { useState, useEffect }  from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableHighlight, Dimensions } from 'react-native';
 import SurveyQuestion from '../components/survey-question';
+import * as Progress from 'react-native-progress';
 
 function Survey(props){
+    const windowWidth = Dimensions.get('window').width;
     const [selectedId, setSelectedId] = useState(0); //reference current question
     const [ref, setRef] = useState(null) //used to reference flatlist position
-    const questionData = [
-        {
-            id: 0,
-            title: "Little interest or pleasure in doing things?"
-        }, 
-        {
-            id: 1,
-            title: "Feeling down, depressed, or hopeless?"    
-        },
-        {
-            id: 2,
-            title: "Trouble falling or staying asleep, or sleeping too much?"    
-        },
-        {
-            id: 3,
-            title: "Feeling tired or having little energy?"    
-        },
-        {
-            id: 4,
-            title: "Poor appetite or overeating?"    
-        },
-        {
-            id: 5,
-            title: "Feeling bad about yourself — or that you are a failure or have let yourself or your family down?"    
-        },
-        {
-            id: 6,
-            title: "Trouble concentrating on things, such as reading the newspaper or watching television?"    
-        },
-        {
-            id: 7,
-            title: "Moving or speaking so slowly that other people could have noticed? Or so fidgety or restless that you have been moving a lot more than usual?"    
-        },
-        {
-            id: 8,
-            title: "Thoughts that you would be better off dead, or thoughts of hurting yourself in some way?"    
-        },
-
-    ]
-    const styles = StyleSheet.create({
-        container: {
-            margin: 0,
-            padding: 0,
-            marginTop: 70,
-            flex: 1
-        }, 
-        title: {
-            fontSize: 30,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            marginTop: 20,
-        },
-        questionIntro: {
-            marginTop: 15,
-            padding: 10,
-            fontSize: 20,
-            alignSelf: 'center',
-            backgroundColor: 'rgb(200,200,200)',
-            width: '100%',
-            textAlign: 'center',
-        },
-        flexContainer: {
-            flex: 1, 
-            flexDirection: 'row',
-            justifyContent: 'center', 
-            flexWrap: 'wrap',
-            marginTop: 20,
-        },
-        button: {
-            height: 150,
-            width: 150,
-            borderRadius: 10,
-            backgroundColor: 'skyblue',
-            margin: 8,
-        },
-        buttonContent: {
-            textAlign: 'center',
-            padding: 15,
-            paddingTop: 20,
-            fontSize: 20
-        },
-        backButton: {
-            position: 'absolute',
-            bottom: 15,
-            left: 15,
-            width: 130,
-            height: 50,
-            backgroundColor: 'skyblue',
-            flex: 1,
-            justifyContent: 'center',
-            borderRadius: 10
-        },
-        backText: {
-            textAlign: 'center',
-            color: 'white',
-            fontWeight: 'bold',
-        }
-    });
 
     const renderItem = ({ item }) => {
         return <SurveyQuestion questionTitle={item.title} press={scrollForward}/>
@@ -117,7 +21,6 @@ function Survey(props){
     const scrollBack = () => {
         if(selectedId > 0){
             setSelectedId(selectedId-1);
-            // ref.scrollToIndex({index: selectedId});
         }
     }
 
@@ -131,18 +34,123 @@ function Survey(props){
         <View style={styles.container}>
             <Text style={styles.title}>PHQ-9 Survey</Text>
             <Text style={styles.questionIntro} >How often have you been bothered by the following over the past 2 weeks?</Text>
+            <Progress.Bar style={styles.progressBar} progress={selectedId/questionData.length} width={windowWidth} height={20} color="rgb(26,177,147)" borderRadius={0} borderWidth={0}/>
             <FlatList data={questionData} 
             ref={(ref)=>{setRef(ref)}}
             renderItem={renderItem} 
             keyExtractor={(item) => item.id}
             horizontal={true}
             pagingEnabled={true}
-            scrollEnabled={true}/>
+            scrollEnabled={false}/>
             <TouchableHighlight style={styles.backButton} onPress={scrollBack}>
                 <Text style={styles.backText}>Back</Text>
             </TouchableHighlight>
         </View>
     );
 }
+
+const questionData = [
+    {
+        id: 0,
+        title: "Little interest or pleasure in doing things?"
+    }, 
+    {
+        id: 1,
+        title: "Feeling down, depressed, or hopeless?"    
+    },
+    {
+        id: 2,
+        title: "Trouble falling or staying asleep, or sleeping too much?"    
+    },
+    {
+        id: 3,
+        title: "Feeling tired or having little energy?"    
+    },
+    {
+        id: 4,
+        title: "Poor appetite or overeating?"    
+    },
+    {
+        id: 5,
+        title: "Feeling bad about yourself — or that you are a failure or have let yourself or your family down?"    
+    },
+    {
+        id: 6,
+        title: "Trouble concentrating on things, such as reading the newspaper or watching television?"    
+    },
+    {
+        id: 7,
+        title: "Moving or speaking so slowly that other people could have noticed? Or so fidgety or restless that you have been moving a lot more than usual?"    
+    },
+    {
+        id: 8,
+        title: "Thoughts that you would be better off dead, or thoughts of hurting yourself in some way?"    
+    },
+
+]
+const styles = StyleSheet.create({
+    container: {
+        margin: 0,
+        padding: 0,
+        marginTop: 70,
+        flex: 1
+    }, 
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    questionIntro: {
+        marginTop: 15,
+        padding: 10,
+        fontSize: 20,
+        alignSelf: 'center',
+        backgroundColor: 'rgb(200,200,200)',
+        width: '100%',
+        textAlign: 'center',
+    },
+    flexContainer: {
+        flex: 1, 
+        flexDirection: 'row',
+        justifyContent: 'center', 
+        flexWrap: 'wrap',
+        marginTop: 20,
+    },
+    button: {
+        height: 150,
+        width: 150,
+        borderRadius: 10,
+        backgroundColor: 'skyblue',
+        margin: 8,
+    },
+    buttonContent: {
+        textAlign: 'center',
+        padding: 15,
+        paddingTop: 20,
+        fontSize: 20
+    },
+    backButton: {
+        position: 'absolute',
+        bottom: 15,
+        left: 15,
+        width: 130,
+        height: 50,
+        backgroundColor: 'skyblue',
+        flex: 1,
+        justifyContent: 'center',
+        borderRadius: 10
+    },
+    backText: {
+        textAlign: 'center',
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    progressBar: {
+        // position: 'absolute',
+        // bottom: 35,
+        // right: 15,
+    }
+});
 
 export default Survey;
