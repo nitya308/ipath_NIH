62485bc63bed098697c3bb9bf7eb6bb3c4c3df89
nitya-cs <nitya.agarwala@gmail.com>
2022-10-30 17:16:09 -0400

    removed header and restyled survey page

--- src/components/survey/survey-intro.js
+++ src/components/survey/survey-intro.js
@@ -1,82 +1,86 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight, Dimensions } from 'react-native';
+import Clipboard from '../../assets/icons/clipboard';
 
-function SurveyIntro(props){
-    return(
-        <View style={styles.container}>
-            <View style={styles.flexContainer}>
-                <Text style={styles.title}>PHQ-9</Text>
-                <Text style={styles.subTitle}>Depression Screening Survey</Text>
-                <Text style={styles.infoText}>This survey is 9 questions and is a reliable way to determine whether you are displaying symptoms of depression. There are lots of treatments and options to help. We will walk you through these after the survey.</Text>
-            </View>
-            <TouchableHighlight underlayColor="gray" style={styles.startButton} onPress={props.transition}>
-                <Text style={styles.buttonText}>Get Started</Text>
-            </TouchableHighlight>
+function SurveyIntro(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.flexContainer}>
+        <View style={styles.iconConatienr}>
+          <Clipboard width={80} height={80} strokeColor="#5451F8" />
         </View>
-    )
+        <Text style={styles.title}>PHQ-9</Text>
+        <Text style={styles.subTitle}>Depression Screening Survey</Text>
+        <Text style={styles.infoText}>This survey is 9 questions and is a reliable way to determine whether you are displaying symptoms of depression. There are lots of treatments and options to help. We will walk you through these after the survey.</Text>
+      </View>
+      <TouchableHighlight underlayColor="gray" style={styles.startButton} onPress={props.transition}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableHighlight>
+    </View>
+  )
 }
 const windowWidth = Dimensions.get('window').width;
-const windowHeight= Dimensions.get('window').height;
+const windowHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
-    container: {
-        margin: 0,
-        paddingTop: 50,
-        paddingBottom: 50,
-        width: windowWidth,
-        height: windowHeight * .8,
-        backgroundColor: '#373737',
-    }, 
-    flexContainer:{
-        flex: 0, 
-        alignItems: 'flex-start',
-        paddingLeft: 30
+  container: {
+    margin: 0,
+    paddingTop: 50,
+    paddingBottom: 50,
+    width: windowWidth,
+    height: windowHeight,
+    backgroundColor: '#373737',
+  },
+  iconConatienr: {
+    alignSelf: 'flex-end',
+    margin: 50
+  },
+  flexContainer: {
+    flex: 0,
+    alignItems: 'flex-start',
+    paddingLeft: 30
+  },
+  title: {
+    fontSize: 30,
+    padding: 10,
+    color: 'white',
+  },
+  subTitle: {
+    fontSize: 40,
+    paddingLeft: 10,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  infoText: {
+    marginTop: 30,
+    padding: 10,
+    fontSize: 17,
+    color: 'white',
+    fontStyle: 'italic',
+  },
+  startButton: {
+    width: windowWidth * .85,
+    marginTop: 100,
+    height: 60,
+    backgroundColor: '#51B5AF',
+    borderRadius: 30,
+    flex: 0,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
     },
-    title:{
-        fontSize: 50,
-        marginTop: 50,
-        padding: 10,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    subTitle:{
-        fontSize: 30,
-        paddingLeft: 10,
-        color: 'white',
-        width: 275,
-        fontWeight: 'bold'
-    },
-    infoText:{
-        marginTop: 30,
-        padding: 10,
-        fontSize: 17,
-        color: 'white',
-        fontStyle: 'italic',
-    },
-    startButton: {
-        width: windowWidth * .85,
-        position: 'absolute',
-        bottom: 100,
-        height: 60,
-        backgroundColor: '#51B5AF',
-        borderRadius: 30,
-        flex: 0,
-        justifyContent: 'center',
-        alignSelf: 'center',
-        shadowColor: "#000",
-        shadowOffset: {
-        width: 0,
-        height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-    },
-    buttonText: {
-        fontSize: 17,
-        padding: 10,
-        textAlign: 'center',
-        color: 'white',
-        fontWeight: 'bold',
-    }
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+  buttonText: {
+    fontSize: 17,
+    padding: 10,
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+  }
 });
 
 export default SurveyIntro; 
--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -17,72 +17,74 @@ const auth = firebase.auth();
 const Stack = createStackNavigator();
 
 function RootNavigator(props) {
-    const { user, setUser } = useContext(AuthenticatedUserContext);
-    const [isLoading, setIsLoading] = useState(true);
-    
-    useEffect(() => {
-        if (user) {
-            props.loginUser({email: user.email, id: user.uid});
-            props.fetchSavedTreatments(user.uid);
-            props.fetchLastSurveyed(user.uid);
-            props.fetchFirstName(user.uid);
-        } else {
-            props.logoutUser();
-        }
-    },[user])
-    useEffect(() => {
-      // onAuthStateChanged returns an unsubscriber
-      const unsubscribeAuth = auth.onAuthStateChanged(async authenticatedUser => {
-        try {
-          await (authenticatedUser ? setUser(authenticatedUser) : setUser(null));
-          setIsLoading(false);
-        } catch (error) {
-          console.log(error);
-        }
-      });
-  
-      // unsubscribe auth listener on unmount
-      return unsubscribeAuth;
-    }, []);
-  
-    if (isLoading) {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <ActivityIndicator size='large' />
-        </View>
-      );
-    }
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const headerOptions = ({navigation, route}) => ({
-        headerTitle: "iPath",
-        headerStyle: headerStyling,
-        headerRight: () => <ProfileButton navigate={() => navigation.navigate("Profile")}/>,
-        headerTitleContainerStyle: headerTitleContainerStyling,
-        headerBackgroundContainerStyle: {
-          margin:0,
-          padding: 0
-        }
-      });
-      const windowHeight= Dimensions.get('window').height;
-    
-    const headerStyling = {
-      backgroundColor: '#F9FBFB',
-      height: windowHeight * .12,
-      };
-    
-      const headerTitleContainerStyling = {
-        padding: 0,
-        margin: 0,
+  useEffect(() => {
+    if (user) {
+      props.loginUser({ email: user.email, id: user.uid });
+      props.fetchSavedTreatments(user.uid);
+      props.fetchLastSurveyed(user.uid);
+      props.fetchFirstName(user.uid);
+    } else {
+      props.logoutUser();
+    }
+  }, [user])
+  useEffect(() => {
+    // onAuthStateChanged returns an unsubscriber
+    const unsubscribeAuth = auth.onAuthStateChanged(async authenticatedUser => {
+      try {
+        await (authenticatedUser ? setUser(authenticatedUser) : setUser(null));
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
       }
+    });
+
+    // unsubscribe auth listener on unmount
+    return unsubscribeAuth;
+  }, []);
+
+  if (isLoading) {
     return (
-      <NavigationContainer>
-        {user ? <Stack.Navigator initialRouteName='Home' >
-          <Stack.Screen name="HomePage" component={MainTabBar} options={headerOptions}/>
-          <Stack.Screen name="Profile" component={Profile} options={headerOptions}/>
-        </Stack.Navigator> : <HomePages />}
-      </NavigationContainer>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' />
+      </View>
     );
   }
 
+  const headerOptions = ({ navigation, route }) => ({
+    headerTitle: "iPath",
+    headerStyle: headerStyling,
+    headerRight: () => <ProfileButton navigate={() => navigation.navigate("Profile")} />,
+    headerTitleContainerStyle: headerTitleContainerStyling,
+    headerBackgroundContainerStyle: {
+      margin: 0,
+      padding: 0
+    }
+  });
+  const windowHeight = Dimensions.get('window').height;
 
-export default connect(null, { loginUser, logoutUser, fetchSavedTreatments, fetchLastSurveyed, fetchFirstName } )(RootNavigator);
+  const headerStyling = {
+    backgroundColor: '#F9FBFB',
+    height: windowHeight * .12,
+  };
+
+  const headerTitleContainerStyling = {
+    padding: 0,
+    margin: 0,
+  }
+  return (
+    <NavigationContainer>
+      {user ? <Stack.Navigator initialRouteName='Home' screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen name="HomePage" component={MainTabBar} options={headerOptions} />
+        <Stack.Screen name="Profile" component={Profile} options={headerOptions} />
+      </Stack.Navigator> : <HomePages />}
+    </NavigationContainer>
+  );
+}
+
+
+export default connect(null, { loginUser, logoutUser, fetchSavedTreatments, fetchLastSurveyed, fetchFirstName })(RootNavigator);
