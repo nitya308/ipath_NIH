1b647b16768b2d2a07d7c441a469a8724edc6d66
burkejaeger <81589723+burkejaeger@users.noreply.github.com>
2022-11-20 00:41:40 -0500

    updated styling of main tab bar

--- src/navigation/main_tab_bar.js
+++ src/navigation/main_tab_bar.js
@@ -22,7 +22,7 @@ function MainTabBar(props){
   return (
       <Tab.Navigator
       screenOptions={{
-        tabBarActiveTintColor: "#469C97",
+        tabBarActiveTintColor: "#5451F8",
         tabBarStyle: [
           {
             "display": "flex",
